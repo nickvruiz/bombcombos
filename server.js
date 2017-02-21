29290e16df9abd90fffad6926d9401e024f4364b
nick <nick@steelhouse.com>
2017-02-20 23:30:36 -0800

    add nodemon, setup first route to get bombcombos "things"

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express');
+const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
-MongoClient.connect('mongodb://localhost:27017/things', (err, db) => {
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect('mongodb://localhost:27017/bombcombos', (err, db) => {
 	if (err) {
 		console.trace(err);
+		return false;
 	}
 
-	db.things.find().toArray((err, res) => {
-		if (err) {
-			console.trace(err);
-		}
+	app.get('/things', (req, res) => {
+		db.collection('things').find({}).toArray((err, data) => {
+			if (err) {
+				console.trace(err);
+				return false;
+			}
 
-		console.log('res ->', res);
+			res.send(data);
+		});
 	});
-});
 
-app.get('/ping', (req, res) => {
-	console.log('pong');
-});
-
-const SERVER_PORT = 3000;
-
-app.listen(SERVER_PORT, () => {
-	console.log(`Listening on port ${SERVER_PORT}`);
+	// Start server
+	const SERVER_PORT = 3000;
+	app.listen(SERVER_PORT, () => {
+		console.log(`Listening on port ${SERVER_PORT}`);
+	});
 });
