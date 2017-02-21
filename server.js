ea2b7c7ce5d1264f8871b2d1a5fcf29ce4615144
nick <nick@steelhouse.com>
2017-02-20 22:55:06 -0800

    add mongo entrypoint

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
+MongoClient.connect('mongodb://localhost:27017/things', (err, db) => {
+	if (err) {
+		console.trace(err);
+	}
+
+	db.things.find().toArray((err, res) => {
+		if (err) {
+			console.trace(err);
+		}
+
+		console.log('res ->', res);
+	});
+});
+
 app.get('/ping', (req, res) => {
 	console.log('pong');
 });
